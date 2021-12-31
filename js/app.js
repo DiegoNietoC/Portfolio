c463372f22d2c6d94f6ee1a5625d17cf63e6857c
Diego <diego.n.x.000@gmail.com>
2021-12-30 22:53:04 -0600

    Finished portfolio

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ var x = "o";
 var o = "";
 var correo = "diego.n.x.000@gmail.com" 
 
+//Textos de idioma por defecto
 document.getElementById("titulo").innerHTML = `${nombre} Nieto Conrado`;
 document.getElementById("nombre_").innerHTML = `${nombre.toUpperCase()} NIETO CONRADO`;
 document.getElementById("textAboutMe").innerHTML = `I'm ${nombre}, Front End web developer. I like to design modern and minimalist applications using HTML, CSS and Javascript. <br><br>
 	Besides web development, I am interested in other fields of technology, such as robotics, electronics, and artificial intelligence. <br><br>
 	When I'm not working in programming and development, I play my artistic side by drawing and illustrating with programs like Adobe Photoshop, Illustrator, and Premiere Pro. <br><br>
 	I like simple and elegant solutions. I am a self-taught person and always ready to learn new technologies.`;
-document.getElementById("downloadCVlink").href = `archivos/${nombre}NC-CV-eng.pdf`;
+document.getElementById("downloadCVlink").href = `https://10ne-v.neocities.org/Portfolio/assets/${nombre}NC-CV-eng.pdf`;
 document.getElementById("mail").href = `mailto:${correo}`;
 document.getElementById("mail").innerHTML = correo;
 
@@ -40,7 +41,7 @@ function engFxText(){
 	document.getElementById("works").innerHTML = "Works";
 	document.getElementById("portfolio4").innerHTML = "Portfolio";
 	document.getElementById("downloadCVbutton").innerHTML = "Download CV";
-	document.getElementById("downloadCVlink").href = `archivos/${nombre}NC-CV-eng.pdf`;
+	document.getElementById("downloadCVlink").href = `https://10ne-v.neocities.org/Portfolio/assets/${nombre}NC-CV-eng.pdf`;
 	document.getElementById("textContact").innerHTML = "Contact";
 	document.getElementById("YouCanWriteMe").innerHTML = "You can write to me at:";
 }
@@ -62,7 +63,7 @@ function espFxText(){
 	document.getElementById("works").innerHTML = "Trabajos";
 	document.getElementById("portfolio4").innerHTML = "Portafolio";
 	document.getElementById("downloadCVbutton").innerHTML = "Descargar CV";
-	document.getElementById("downloadCVlink").href = `archivos/${nombre}NC-CV-esp.pdf`;
+	document.getElementById("downloadCVlink").href = `https://10ne-v.neocities.org/Portfolio/assets/${nombre}NC-CV-esp.pdf`;
 	document.getElementById("textContact").innerHTML = "Contacto";
 	document.getElementById("YouCanWriteMe").innerHTML = "Puedes escribirme a:";
 }
